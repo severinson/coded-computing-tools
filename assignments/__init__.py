70ee6e35b6fa6f91b387aca024a1708667d1ccae
Albin Severinson <albin@severinson.org>
2017-05-09 15:02:32 +0200

    License header and comments.

--- assignments/__init__.py
+++ assignments/__init__.py
@@ -1,6 +1,22 @@
+############################################################################
+# Copyright 2016 Albin Severinson                                          #
+#                                                                          #
+# Licensed under the Apache License, Version 2.0 (the "License");          #
+# you may not use this file except in compliance with the License.         #
+# You may obtain a copy of the License at                                  #
+#                                                                          #
+#     http://www.apache.org/licenses/LICENSE-2.0                           #
+#                                                                          #
+# Unless required by applicable law or agreed to in writing, software      #
+# distributed under the License is distributed on an "AS IS" BASIS,        #
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. #
+# See the License for the specific language governing permissions and      #
+# limitations under the License.                                           #
+############################################################################
+
 '''An assignment stores which partitions are stored in which batch,
-and at what servers. Ths init file contains the assignment abstract
-base class.
+and at what servers. This file contains the assignment abstract base
+class.
 
 '''
 
@@ -10,7 +26,7 @@ class AssignmentError(Exception):
     '''Base class for exceptions thrown by this package.'''
 
 class Assignment(ABC):
-    '''Abstract superclass representing an assignment of encoded rows into
+    '''Abstract base class representing an assignment of encoded rows into
     batches.
 
     '''
