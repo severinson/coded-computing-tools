bffae8fcc75ba096f2f7270d283c425c0ccb153b
Albin Severinson <albin@severinson.org>
2017-10-10 19:30:41 +0200

    added numinv from pyrateless repo

--- numtools.py
+++ numtools.py
@@ -7,8 +7,74 @@ binary search.
 
 import math
 
+from functools import partial
+
+def _find_limits(fun=None, target=None):
+    '''bound the input needed to reach value'''
+    assert callable(fun)
+    assert target is not None
+    value = 1
+    while fun(value) < target:
+        value *= 2
+    return math.floor(value / 2), value
+
+def _bounds_wrap(value, fun=None, lower=None, upper=None):
+    '''wrap a function with bounds checking'''
+    if value < lower:
+        return -math.inf
+    if value > upper:
+        return math.inf
+    return fun(value)
+
+def numinv(fun=None, target=None, lower=None, upper=None):
+    '''numerically invert a discrete function
+
+    args:
+
+    fun: function to invert. must be monotonically increasing in the range
+    lower to upper. use lambdas to wrap your function if this is not the case.
+
+    target: target value
+
+    lower: lower bound to search, < math.inf or None to find automatically.
+
+    upper: upper bound to search, >= 0, or None to find automatically.
+
+    returns: value such that fun(value) <= target < fun(value+1).
+
+    '''
+    assert callable(fun)
+    assert target is not None
+    assert 0 <= target < math.inf
+    if lower is None:
+        lower = 0
+    if upper is None:
+        upper = math.inf
+
+    # wrap fun with bounds checking
+    ffun = partial(_bounds_wrap, fun=fun, lower=lower, upper=upper)
+
+    # find upper and lower bounds
+    lower, upper = _find_limits(ffun, target)
+
+    # binary search
+    while upper - lower > 1:
+        middle = math.floor(lower + (upper - lower) / 2)
+        if ffun(middle) >= target:
+            upper = middle
+        else:
+            lower = middle
+
+    # return whichever is closest and lower if it is a tie
+    if abs(ffun(lower) - target) <= abs(ffun(upper) - target):
+        return lower
+    else:
+        return upper
+
 def numerical_inverse(fun, value, min_value, max_value, eps=0.00000001, discrete=True):
-    '''Numerically evaluate the inverse of a function by performing binary
+    '''Legacy code. Use the above numinv function instead.
+
+    Numerically evaluate the inverse of a function by performing binary
     search.
 
     Args:
