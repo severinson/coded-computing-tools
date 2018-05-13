8a2754bc7b80c5dd456deb65ce852c71bc8fdb83
Albin Severinson <albin@severinson.org>
2018-05-13 21:42:03 +0200

    tcom plots script

---
+++ tcom_plots.py
@@ -0,0 +1,6 @@
+# import plot settings
+# import evaluator functions
+# import parameters
+# figure out which LT code parameters we need to simulate
+# run LT codes, R10 codes, and RQ codes overnight
+# create one function for each plot
